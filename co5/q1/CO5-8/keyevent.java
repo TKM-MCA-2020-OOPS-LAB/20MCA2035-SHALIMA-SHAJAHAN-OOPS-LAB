a199e24ebfcd7147c1647676d830664e5bf52090
2032 Shalima Shajahan <74792666+Shalima2209@users.noreply.github.com>
2021-09-09 12:44:12 +0530

    .

---
+++ co5/q1/CO5-8/keyevent.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+public class keyevent extends Applet implements KeyListener
+{
+String msg="Type Here..!";
+int x=30,y=50;
+public void init()
+{
+addKeyListener(this);
+requestFocus();
+}
+public void keyTyped(KeyEvent ke)
+{
+msg+=ke.getKeyChar();
+repaint();
+}
+public void keyReleased(KeyEvent ke)
+{
+showStatus("Key Up!");
+}
+public void keyPressed(KeyEvent ke)
+{
+showStatus("Key Down!");
+}
+public void paint(Graphics G)
+{
+G.drawString(msg,x,y);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*<applet code="keyevent.class" width=200 height=300></applet>*/
